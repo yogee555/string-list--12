687a579a6d681e15ad37295f32f849564ee6e82b
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-05 11:23:31 +0530

    Add files via upload

---
+++ string(list)-12.py
@@ -0,0 +1,5 @@
+# popping the last element
+L1 = [1,'a',2,'b',3,'c']   # store value in L1
+print(L1)                  # print L1 store
+L1.pop( )                  # pop() is operated remove 1 last element
+print(L1)                  # execute L1 for remove last 1 value
